667bf5bcda3b997a7f4375633f31e0d63cc5900a
Nikita Semenov <45874635+NikitaSemenovV@users.noreply.github.com>
2023-03-24 10:13:22 +0300

    fix bugs

--- new-project/tests/login.spec.ts
+++ new-project/tests/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-	await page.goto('https://rahulshettyacademy.com/client');
+	await page.goto('/client');
 	await page.locator('#userEmail').fill('nikita4semenov@gmail.com');
 	await page.locator('#userPassword').fill('123456');
 	await page.locator('#login').click();
