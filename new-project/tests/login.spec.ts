7719ae5016decd4c670d7f1af540e7d2ae2700a3
Nikita Semenov <45874635+NikitaSemenovV@users.noreply.github.com>
2023-03-24 11:38:14 +0300

    fix_bugs

--- new-project/tests/login.spec.ts
+++ new-project/tests/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('User_authorization', async ({ page }) => {
 	await page.goto('/client');
 	await page.locator('#userEmail').fill('nikita4semenov@gmail.com');
 	await page.locator('#userPassword').fill('123456');
